87eb8451722641fdabfe929b97d5b9ada854877c
s2tray <r202202201jz@jindai.jp>
2022-11-01 16:56:07 +0900

    20221101

--- de12/python/wiki.py
+++ de12/python/wiki.py
@@ -1,18 +1,3 @@
-from json import tool
-from sympy import python
-import wikipedia
-
-# キーワードを設定
-keyword = "犬"
-# キーワードで検索
-wikipedia.set_lang("ja")
-result = wikipedia.search(keyword)
-print("検索結果",result)
-
-print("最初の検索結果を表示")
-page_data = wikipedia.page(result[0])
-print(page_data.content)
-
 from cv2 import * 
 from numpy import array, where, ones_like
 from matplotlib import pyplot
